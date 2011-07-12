49a370e54b9449130e5b9c109a70bd839566d2fb
David Anderson <dave@halogensunlight.co.uk>
2011-07-12 12:10:27 +0100

    here we go!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedbkr::Application.config.secret_token = 'b686a27e9c159139e340f962227ce5280a29319d2d8fcf7f231d133655912ec4036faff7161cb6f7de867316e2f8afe767c38cadb46027ecc4c81c93bfa4b3e2'
